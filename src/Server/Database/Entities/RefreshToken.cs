f8e2357fc96cfc335bc2cc7550aa22330c2f7a26
MaxMeyer-05 <maxmeyer2411@gmail.com>
2026-09-18 15:10:17 +0200

    Add RefreshToken and User entity classes with necessary properties and annotations

--- src/Server/Database/Entities/RefreshToken.cs
+++ src/Server/Database/Entities/RefreshToken.cs
@@ -1,37 +1,58 @@
+using Microsoft.EntityFrameworkCore;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Server.Database.Entities;
 
 /// <summary>
 /// Represents a refresh token used for obtaining new access tokens.
 /// </summary>
+[Table("refresh_tokens")]
+[Index(nameof(Token), IsUnique = true)]
 public class RefreshToken
 {
     /// <summary>
     /// Defines the unique identifier for the refresh token.
     /// </summary>
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
 
     /// <summary>
     /// Defines the token string for the refresh token.
     /// </summary>
-    public string Token { get; set; } = string.Empty;
+    [Required]
+    public string Token { get; set; } = null!;
 
     /// <summary>
     /// Defines the user identifier associated with the refresh token.
     /// </summary>
+    [Required]
     public Guid UserId { get; set; }
 
     /// <summary>
     /// Defines the expiration date and time (in UTC) of the refresh token.
     /// </summary>
+    [Required]
     public DateTime ExpiresAtUtc { get; set; }
 
     /// <summary>
     /// Indicates whether the refresh token has been revoked.
     /// </summary>
-    public bool IsRevoked { get; set; }
+    [Required]
+    public bool IsRevoked { get; set; } = false;
 
     /// <summary>
     /// Defines the creation date and time (in UTC) of the refresh token.
     /// </summary>
+    [Required]
     public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+
+    /// <summary>
+    /// Defines the user entity associated with the refresh token.
+    /// </summary>
+    [Required]
+    [ForeignKey("UserId")]
+    public User User { get; set; } = null!;
 }
---
+++ src/Server/Database/Entities/User.cs
@@ -0,0 +1,64 @@
+using Microsoft.EntityFrameworkCore;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using Server.Security;
+
+namespace Server.Database.Entities;
+
+/// <summary>
+/// Represents a user entity in the database.
+/// </summary>
+[Table("users")]
+[Index(nameof(UserEmail), IsUnique = true)]
+public class User
+{
+    /// <summary>
+    /// Defines the unique identifier for the user.
+    /// </summary>
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Defines the email of the user.
+    /// </summary>
+    [Required]
+    [EmailAddress]
+    public string UserEmail { get; set; } = null!;
+
+    /// <summary>
+    /// Defines the hashed password of the user.
+    /// </summary>
+    [Required]    
+    public string PasswordHash { get; set; } = null!;
+
+    /// <summary>
+    /// Defines the role of the user.
+    /// </summary>
+    [Required]
+    public Roles Role { get; set; }
+
+    /// <summary>
+    /// Defines the scopes allowed for the user.
+    /// </summary>
+    public IEnumerable<string> AllowedScopes { get; set; } = [];
+
+    /// <summary>
+    /// Defines whether the user is active.
+    /// </summary>
+    [Required]
+    public bool IsActive { get; set; } = true;
+
+    /// <summary>
+    /// Defines the date and time when the user was created.
+    /// </summary>
+    [Required]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    /// <summary>
+    /// Defines the refresh tokens associated with the user.
+    /// </summary>
+    public ICollection<RefreshToken> RefreshTokens { get; set; } = [];
+}
